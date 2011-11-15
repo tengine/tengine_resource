9b24fb813a9539934850e8a4f004daca38b79605
Takahisa Tokunaga <tokunaga.jp@gmail.com>
2011-11-15 15:14:14 +0900

    [tengine_resourceの実装] 起動時の初期処理に関する単純なテストと実装を行いました。

--- lib/tengine/resource/watcher.rb
+++ lib/tengine/resource/watcher.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 require 'eventmachine'
 require 'mongoid'
-
+require 'apis/wakame'
 require 'tengine/core/config'
 
 class Tengine::Resource::Watcher
@@ -11,6 +11,16 @@ class Tengine::Resource::Watcher
   def initialize(argv = [])
     @config = Tengine::Core::Config.parse(argv)
     @pid = sprintf("process:%s/%d", ENV["MM_SERVER_NAME"], Process.pid)
+    @daemonize_options = {
+      :app_name => 'tengine_resource_watchd',
+      :ARGV => ['start'],
+      :ontop => !@config[:tengined][:daemon],
+      :monitor => !@config[:tengined][:monitor],
+      :multiple => true,
+      :dir_mode => :normal,
+      :dir => File.expand_path(@config[:tengined][:pid_dir]),
+    }
+    @daemonize_options.update(:ARGV => ['start'])
     Tengine::Core::MethodTraceable.disabled = !@config[:verbose]
   rescue Exception
     puts "[#{$!.class.name}] #{$!.message}\n  " << $!.backtrace.join("\n  ")
@@ -45,10 +55,78 @@ class Tengine::Resource::Watcher
 
   def init_process
     sender.wait_for_connection do
+      # Wakameのプロバイダを対象として取得
+      providers = Provider::Wakame.all
+      providers.each do |provider|
+        # APIからの仮想サーバタイプ情報を取得
+        instance_specs = []
+        provider.connection do |conn|
+          instance_specs = JSON.parse(conn.show_instance_specs([]))
+        end
+
+        exists_instance_specs = []
+        create_server_types = []
+        update_server_types = []
+        destroy_server_types = []
+
+        # 仮想イメージタイプの取得
+        old_server_types = provider.virtual_server_types
+        old_server_types.each do |old_server_type|
+          instance_spec = instance_specs.detect { |instance_spec| instance_spec["id"] == old_server_type.provided_id }
+
+          if instance_spec
+            # Tengine上に存在するサーバタイプを保持
+            exists_instance_specs << instance_spec
+
+            # APIで取得したサーバタイプと一致するものがあれば更新対象
+            # さらに更新対象は差分があるかの比較
+            update_server_type = needs_to_update_server_type(old_server_type, instance_spec)
+            updata_server_types << update_server_type if update_server_type
+          else
+            # APIで取得したサーバタイプと一致するものがなければ削除対象
+            destroy_server_types << old_server_type
+          end
+        end
+        # APIで取得したサーバタイプがTengine上に存在しないものであれば登録対象
+        create_server_types = instance_specs - exists_instance_specs
+        create_server_types.map(:ServerType.convert)
+
+        # 更新
+        provider.update_virtual_server_types(update_server_types) unless update_server_types.empty?
+        # 登録
+        provider.create_virtual_server_types(create_server_types) unless create_server_types.empty?
+        # 削除
+        provicer.destroy_virtual_server_types(destroy_server_types) unless destroy_server_types.empty?
+      end
     end
   end
 
   extend Tengine::Core::MethodTraceable
   method_trace(*instance_methods(false))
+
+  private
+
+  def needs_to_update_server_type(old_server_type, instance_spec)
+    return nil unless instance_spec
+
+    old_server_type.provided_id = instance_spec.delete("id")
+    old_server_type.caption = instance_spec.delete("uuid")
+    old_server_type.cpu_cores = instance_spec.delete("cpu_cores")
+    old_server_type.memory_sites = instance_spec.delete("memory_size")
+    old_server_type.properties.update(instance_spec)
+    # :id
+    # :uuid
+    # :account_id
+    # :cpu_cores
+    # :memory_size
+    # :arch
+    # :hypervisor
+    # :drives
+    # :vifs
+    # :quota_weight
+    # :created_at
+    # :updated_at
+    return old_server_type if old_server_type.changed?
+  end
 
 end
--- spec/tengine/resource/watcher_spec.rb
+++ spec/tengine/resource/watcher_spec.rb
@@ -8,7 +8,7 @@ describe Tengine::Resource::Watcher do
   before do
   end
 
-  context :initialize do
+  describe :initialize do
     it "default" do
       Tengine::Core::MethodTraceable.stub(:disabled=)
       watcher = Tengine::Resource::Watcher.new
@@ -22,7 +22,8 @@ describe Tengine::Resource::Watcher do
     end
   end
 
-  context :start do
+  describe :start do
+
     before do
       @watcher = Tengine::Resource::Watcher.new
       EM.should_receive(:run).and_yield
@@ -34,6 +35,7 @@ describe Tengine::Resource::Watcher do
     end
 
     describe :init_process do
+
       before do
         # コネクションの mock を生成
         mock_conn = mock(:connection)
@@ -46,28 +48,220 @@ describe Tengine::Resource::Watcher do
         mock_conn.should_receive(:on_closed)
       end
 
-      it "生成したmq_suiteが設定されている" do
-        mock_mq = Tengine::Mq::Suite.new(@watcher.config[:event_queue])
-        Tengine::Mq::Suite.should_receive(:new).
-          with(@watcher.config[:event_queue]).and_return(mock_mq)
+      describe :sender do
 
-        @watcher.sender.should_receive(:wait_for_connection)
-        @watcher.start
+        it "生成したmq_suiteが設定されている" do
+          mock_mq = Tengine::Mq::Suite.new(@watcher.config[:event_queue])
+          Tengine::Mq::Suite.should_receive(:new).
+            with(@watcher.config[:event_queue]).and_return(mock_mq)
+
+          @watcher.sender.should_receive(:wait_for_connection)
+          @watcher.start
+          @watcher.mq_suite.should == mock_mq
+        end
+
+        it "生成したsenderが設定されている", :failure => true do
+          mock_mq = Tengine::Mq::Suite.new(@watcher.config[:event_queue])
+          Tengine::Mq::Suite.should_receive(:new).
+            with(@watcher.config[:event_queue]).and_return(mock_mq)
+
+          mock_sender = mock(:sender)
+          Tengine::Event::Sender.should_receive(:new).with(mock_mq).and_return(mock_sender)
+
+          @watcher.sender.should_receive(:wait_for_connection)
+          @watcher.start
+          @watcher.sender.should == mock_sender
+        end
+
+        context "wakame" do
+          before do
+            Tengine::Resource::Provider.delete_all
+#             @provider_ec2 = Tengine::Resource::Provider::Ec2.create!({
+#                 :name => "amazon-ec2",
+#                 :description => "",
+#                 :properties => {
+#                 },
+#                 :provider_inquiry_interval => 30,
+#                 :provider_heartbeat_interval => 30,
+#                 :connection_settings => {
+#                   :access_key => "",
+#                   :secret_access_key => "",
+#                   :options => {
+#                     :server => "ec2.amazonaws.com",
+#                     :port => "443",
+#                     :protocol => "https",
+#                     :multi_thread => false,
+#                     :logger => nil,
+#                     :signature_version => '1',
+#                     :cache => false,
+#                   }
+#                 }
+#               })
+            @provider_wakame = Tengine::Resource::Provider::Wakame.create!({
+                :name => "wakame-vdc",
+                :description => "",
+                :properties => {
+                },
+                :provider_inquiry_interval => 30,
+                :provider_heartbeat_interval => 30,
+                :connection_settings => {
+                  :account_id => "test",
+                  :server => "192.168.0.10",
+                  :port => "80",
+                  :protocol => "http",
+                  :user_data => "",
+                  :options => {},
+                },
+              })
+            @virtual_server_type_wakame = @provider_wakame.create_virtual_server_type({
+                :provided_id => "is-demospec",
+                :caption => "is-demospec",
+                :cpu_cores => 2,
+                :memory_size => 512,
+                :properties => {
+                  :arch => "x86_64",
+                  :hypervisor => "kvm",
+                  :account_id => "a-shpoolxx",
+                  :vifs => "--- \neth0: \n  :bandwidth: 100000\n  :index: 0\n",
+                  :quota_weight => "1.0",
+                  :drives => "--- \nephemeral1: \n  :type: :local\n  :size: 100\n  :index: 0\n",
+                  :created_at => "2011-10-28T02:58:57Z",
+                  :updated_at => "2011-10-28T02:58:57Z",
+                }
+              })
+
+            @mock_mq = Tengine::Mq::Suite.new(@watcher.config[:event_queue])
+            Tengine::Mq::Suite.should_receive(:new).
+              with(@watcher.config[:event_queue]).and_return(@mock_mq)
+            Tengine::Event.should_receive(:mq_suite).and_return(@mock_mq)
+            Tengine::Event.should_receive(:default_sender).
+              and_return(Tengine::Event::Sender.new(@mock_mq))
+            Tengine::Event::Sender.should_receive(:new).with(@mock_mq)
+          end
+
+          it "apiから仮想サーバタイプの情報が取得できる" do
+            @watcher.sender.should_receive(:wait_for_connection).and_yield
+            api_conn = mock(:tama)
+            api_conn.should_receive(:show_instance_specs).with([]).
+              and_return(RESULT_UPDATE_WAKAME_SHOW_INSTANCE_SPECS)
+            @provider_wakame.should_receive(:connection).and_yield(api_conn)
+            @watcher.start
+          end
+
+          it "更新対象あったら更新完了後イベントを発火する" do
+            @watcher.sender.should_receive(:wait_for_connection).and_yield
+
+            api_conn = mock(:tama)
+            api_conn.should_receive(:show_instance_specs).with([]).
+              and_return(RESULT_UPDATE_WAKAME_SHOW_INSTANCE_SPECS)
+            @provider_wakame.should_receive(:connection).and_yield(api_conn)
+
+            @virtual_server_type_wakame.update({
+                :cpu_cores => 2,
+                :memory_size => 512,
+              })
+            @provider_wakame.should_receive(:update_virtual_servers).
+              with(@virtual_server_type_wakame)
+            Tengine::Event.should_receive(:fire)
+            mock_queue = mock(:queue)
+            mock_mq.should_receive(:queue).and_return(mock_queue)
+            # mock_mq.should_receive(:wait_for_connection)
+
+            @watcher.start
+          end
+
+          it "登録対象があったら登録完了後イベントを発火する" do
+            @watcher.sender.should_receive(:wait_for_connection).and_yield
+
+            api_conn = mock(:tama)
+            api_conn.should_receive(:show_instance_specs).with([]).
+              and_return(RESULT_CREATE_WAKAME_SHOW_INSTANCE_SPECS)
+            @provider_wakame.should_receive(:connection).and_yield(api_conn)
+
+            @provider_wakame.should_receive(:create_virtual_servers).
+              with([{
+                  :provided_id => "is-demospec2",
+                  :caption => "is-demospec2",
+                  :cpu_cores => 2,
+                  :memory_size => 1024,
+                  :properties => {
+                    :arch => "x86_64",
+                    :hypervisor => "kvm",
+                    :account_id => "b-shpoolxx",
+                    :vifs => "--- \neth0: \n  :bandwidth: 100000\n  :index: 0\n",
+                    :quota_weight => "1.0",
+                    :drives => "--- \nephemeral1: \n  :type: :local\n  :size: 100\n  :index: 0\n",
+                    :created_at => "2011-10-28T02:58:57Z",
+                    :updated_at => "2011-10-28T02:58:57Z",
+                  }
+                }]
+              )
+            Tengine::Event.should_receive(:fire)
+            mock_queue = mock(:queue)
+            mock_mq.should_receive(:queue).and_return(mock_queue)
+            # mock_mq.should_receive(:wait_for_connection)
+
+            @watcher.start
+          end
+
+          it "削除対象があったら削除完了後イベントを発火する" do
+            @watcher.sender.should_receive(:wait_for_connection).and_yield
+
+            api_conn = mock(:tama)
+            api_conn.should_receive(:show_instance_specs).with([]).and_return("")
+            @provider_wakame.should_receive(:connection).and_yield(api_conn)
+
+            @provider_wakame.should_receive(:destroy_virtual_servers).with(@virtual_server_type_wakame)
+            Tengine::Event.should_receive(:fire)
+            mock_queue = mock(:queue)
+            mock_mq.should_receive(:queue).and_return(mock_queue)
+            # mock_mq.should_receive(:wait_for_connection)
+
+            @watcher.start
+          end
+
+        end
       end
-
-      it "生成したsenderが設定されている", :failure => true do
-        mock_mq = Tengine::Mq::Suite.new(@watcher.config[:event_queue])
-        Tengine::Mq::Suite.should_receive(:new).
-          with(@watcher.config[:event_queue]).and_return(mock_mq)
-        mock_sender = mock(:sender)
-        Tengine::Event::Sender.should_receive(:new).with(mock_mq).and_return(mock_sender)
-
-        @watcher.sender.should_receive(:wait_for_connection)
-        @watcher.start
-      end
-
     end
   end
 
+
+RESULT_UPDATE_WAKAME_SHOW_INSTANCE_SPECS = <<-EOS
+[
+  {
+    "cpu_cores": 1,
+    "memory_size": 256,
+    "arch": "x86_64",
+    "hypervisor": "kvm",
+    "updated_at": "2011-10-28T02:58:57Z",
+    "account_id": "a-shpoolxx",
+    "vifs": "--- \neth0: \n  :bandwidth: 100000\n  :index: 0\n",
+    "quota_weight": 1.0,
+    "id": "is-demospec",
+    "created_at": "2011-10-28T02:58:57Z",
+    "drives": "--- \nephemeral1: \n  :type: :local\n  :size: 100\n  :index: 0\n",
+    "uuid": "is-demospec"
+  }
+]
+EOS
+
+RESULT_CREATE_WAKAME_SHOW_INSTANCE_SPECS = <<-EOS
+[
+  {
+    "cpu_cores": 2,
+    "memory_size": 1024,
+    "arch": "x86_64",
+    "hypervisor": "kvm",
+    "updated_at": "2011-10-28T02:58:57Z",
+    "account_id": "b-shpoolxx",
+    "vifs": "--- \neth0: \n  :bandwidth: 100000\n  :index: 0\n",
+    "quota_weight": 1.0,
+    "id": "is-demospec2",
+    "created_at": "2011-10-28T02:58:57Z",
+    "drives": "--- \nephemeral1: \n  :type: :local\n  :size: 100\n  :index: 0\n",
+    "uuid": "is-demospec2"
+  }
+]
+EOS
 
 end
