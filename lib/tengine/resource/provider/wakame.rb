1e184ed9f9ec70a1cf7e65921c76e5039bf15803
akima <akima@nautilus-technologies.com>
2011-11-18 02:05:35 +0900

    [仮想サーバの起動可能数を求めるAPIを実装する] 例は一つだけですが、ひとまず実装しました

--- lib/tengine/resource/provider/wakame.rb
+++ lib/tengine/resource/provider/wakame.rb
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 class Tengine::Resource::Provider::Wakame < Tengine::Resource::Provider::Ec2
+
+  PHYSICAL_SERVER_STATES = [:online, :offline].freeze
+
+  VIRTUAL_SERVER_STATES = [
+    :scheduling, :pending, :starting, :running,
+    :failingover, :shuttingdown, :terminated].freeze
+
   def update_virtual_server_images
     connect do |conn|
       hashs = conn.describe_images.map do |hash|
@@ -43,6 +50,36 @@ class Tengine::Resource::Provider::Wakame < Tengine::Resource::Provider::Ec2
         serv.update_attributes(:status => "shutdown in progress") if serv # <- ?
         serv
       end
+    end
+  end
+
+  def capacities
+    server_type_ids = virtual_server_types.map(&:provided_id)
+    server_type_to_cpu = virtual_server_types.inject({}) do |d, server_type|
+      d[server_type.provided_id] = server_type.cpu_cores
+      d
+    end
+    server_type_to_mem = virtual_server_types.inject({}) do |d, server_type|
+      d[server_type.provided_id] = server_type.memory_size
+      d
+    end
+    physical_servers.inject({}) do |result, physical_server|
+      if physical_server.status == 'online'
+        cpu_free = physical_server.cpu_cores - physical_server.guest_servers.map{|s| server_type_to_cpu[s.provided_type_id]}.sum
+        mem_free = physical_server.memory_size - physical_server.guest_servers.map{|s| server_type_to_mem[s.provided_type_id]}.sum
+        result[physical_server.provided_id] = server_type_ids.inject({}) do |dest, server_type_id|
+          dest[server_type_id] = [
+            cpu_free / server_type_to_cpu[server_type_id],
+            mem_free / server_type_to_mem[server_type_id]
+          ].min
+          dest
+        end
+      else
+        result[physical_server.provided_id] = server_type_ids.inject({}) do |dest, server_type_id|
+          dest[server_type_id] = 0; dest
+        end
+      end
+      result
     end
   end
 
